dd2f6c187c55ea68178874e6fe457496e7f53136
Ilmiddin O'rinov <ilmiddinorinov1@gmail.com>
2024-08-27 10:05:58 +0500

    test game firebase

---
+++ app/src/main/java/uz/ilmiddin1701/test_game/MainActivity.kt
@@ -0,0 +1,19 @@
+package uz.ilmiddin1701.test_game
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import uz.ilmiddin1701.test_game.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
