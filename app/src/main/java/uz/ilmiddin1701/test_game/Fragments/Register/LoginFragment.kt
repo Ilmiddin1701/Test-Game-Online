a04753b9131c0d167eadcfaebecc216b67f207f7
Ilmiddin O'rinov <ilmiddinorinov1@gmail.com>
2024-08-30 09:21:19 +0500

    test game firebase

--- app/src/main/java/uz/ilmiddin1701/test_game/Fragments/Register/LoginFragment.kt
+++ app/src/main/java/uz/ilmiddin1701/test_game/Fragments/Register/LoginFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import uz.ilmiddin1701.test_game.R
@@ -16,8 +18,18 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding.apply {
+            val openAnim = AnimationUtils.loadAnimation(requireContext(), R.anim.open_anim)
+            val exitAnim = AnimationUtils.loadAnimation(requireContext(), R.anim.exit_anim)
+            constraint1.startAnimation(openAnim)
             tvRegister.setOnClickListener {
-                findNavController().navigate(R.id.registerFragment)
+                constraint1.startAnimation(exitAnim)
+                exitAnim.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(p0: Animation?) {}
+                    override fun onAnimationEnd(p0: Animation?) {
+                        findNavController().navigate(R.id.registerFragment)
+                    }
+                    override fun onAnimationRepeat(p0: Animation?) {}
+                })
             }
         }
         return binding.root
--- app/src/main/java/uz/ilmiddin1701/test_game/Fragments/Register/RegisterFragment.kt
+++ app/src/main/java/uz/ilmiddin1701/test_game/Fragments/Register/RegisterFragment.kt
@@ -4,18 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import uz.ilmiddin1701.test_game.R
 import uz.ilmiddin1701.test_game.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
     private val binding by lazy { FragmentRegisterBinding.inflate(layoutInflater) }
+    private lateinit var openAnim: Animation
+    private lateinit var exitAnim: Animation
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding.apply {
+            openAnim = AnimationUtils.loadAnimation(context, R.anim.open_anim)
+            exitAnim = AnimationUtils.loadAnimation(context, R.anim.exit_anim)
+            constraint1.startAnimation(openAnim)
+            requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackPressedCallBack)
+
 
         }
         return binding.root
+    }
+
+    private val onBackPressedCallBack = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            binding.constraint1.startAnimation(exitAnim)
+            exitAnim.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(p0: Animation?) { }
+                override fun onAnimationEnd(p0: Animation?) {
+                    findNavController().popBackStack()
+                }
+                override fun onAnimationRepeat(p0: Animation?) { }
+            })
+        }
     }
 }
